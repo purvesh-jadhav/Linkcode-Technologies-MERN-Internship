1722ab7392e9a8f955a0f2a19d2e8379299c23f5
Purvesh Jadhav <purveshjadhav05@gmail.com>
2026-06-05 22:34:14 +0530

    Added Day-02 tasks

---
+++ Javascript/Day-02/variable.js
@@ -0,0 +1,38 @@
+// redeclaration -np
+// let a=10;
+// let a=20;
+// console.log(a);
+
+// reinitialization -p
+// let b="hello"
+// b="bye"
+// console.log(b)
+
+// hoisting -np
+// c=20;
+// let c;
+// console.log(c)
+
+//redeclaration -np
+// const a=10;
+// const a=20;
+// console.log(a);
+
+// reinitaliazation -np
+// const b=10;
+// b=25; 
+// console.log(b);
+
+//hoisting -np
+// d=55;
+// const d;
+// console.log(d);
+
+var a=10;
+console.log(a)
+
+let b=100
+console.log(b)
+
+const c=240
+console.log(c)
